28d44536efa6d68095cafeea63140d555a3bdf0f
dani <dani16lp@gmail.com>
2021-01-14 21:04:53 +0100

    private messages full

--- src/main.sh
+++ src/main.sh
@@ -1,9 +1,20 @@
 source "$PR_SIZE_LABELER_HOME/src/rocket.sh"
 
+# $1 -> Chat Url
+# $2 -> Message
+# $3 -> Group hook (opt)
+# $4,$5,$6 -> Private message params
+
 main(){
 
-   rocket::sendGroupMessage $1 $2
-   rocket::sendPrivateMessage $3 $4 $5 $2
+export ROCKET_CHAT_URL="$1"
+export BOT_NAME="$7"
+export BOT_AVATAR="$8"
 
+if [[ -n "$5" ]]; then
+  rocket::sendPrivateMessage $4 $5 $6 $2
+else
+  rocket::sendGroupMessage $3 $2
+fi
    exit $?
 }
--- src/rocket.sh
+++ src/rocket.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
-ROCKET_CHAT_URL="https://chat.bulevip.com"
-URL_LOGIN="${ROCKET_CHAT_URL}/api/v1/login"
-SEND_MESSAGE_URL="${ROCKET_CHAT_URL}/api/v1/chat.postMessage"
+URL_LOGIN="/api/v1/login"
+SEND_MESSAGE_URL="/api/v1/chat.postMessage"
 
 rocket::sendGroupMessage(){
 	msg="$2"
   ROCKET_CHAT_HOOK="$1"
-	data="{\"text\":\"${msg}\",\"username\": \"Github Bot\"}"
-	curl -H "Content-type:application/json" "{$ROCKET_CHAT_URL}/hooks/{$ROCKET_CHAT_HOOK}" -d "$data"
+	data="{\"text\":\"${msg}\",\"username\": \"${BOT_NAME}\",\"avatar\":\"${BOT_AVATAR}\"}"
+	curl -H "Content-type:application/json" "${ROCKET_CHAT_URL}/hooks/{$ROCKET_CHAT_HOOK}" -d "$data"
 }
 
 
@@ -17,12 +16,12 @@ rocket::sendPrivateMessage(){
   TO="@$3"
 
   loginData="{\"user\":\"$1\",\"password\":\"$2\"}"
-  data="{\"channel\":\"${TO}\",\"text\":\"${msg}\",\"username\": \"Github Bot\",\"avatar\":\"https://i.pinimg.com/originals/bd/5c/46/bd5c46c4c1579c56c60920301ea3e642.png\"}"
-  login=$(curl -s -H "Content-type:application/json" "{$URL_LOGIN}" -d "$loginData")
+  data="{\"channel\":\"${TO}\",\"text\":\"${msg}\",\"username\": \"${BOT_NAME}\",\"avatar\":\"${BOT_AVATAR}\"}"
+  login=$(curl -s -H "Content-type:application/json" "${ROCKET_CHAT_URL}{$URL_LOGIN}" -d "$loginData")
   userData=$(echo "$login" | jq '.data')
   userId=$(echo "$userData" | jq -r '.userId')
   token=$(echo "$userData" | jq -r '.authToken')
 
-  response=$(curl -s -H "Content-type:application/json" --header "X-User-Id: "$userId --header "X-Auth-Token: "$token "{$SEND_MESSAGE_URL}" -d "$data")
+  response=$(curl -s -H "Content-type:application/json" --header "X-User-Id: "$userId --header "X-Auth-Token: "$token "${ROCKET_CHAT_URL}{$SEND_MESSAGE_URL}" -d "$data")
   echo {$response}
 }
