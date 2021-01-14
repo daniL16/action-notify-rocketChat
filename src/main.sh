d2af7c629236414d626ce167f719072bee8ac7fc
dani <dani16lp@gmail.com>
2021-01-14 14:25:14 +0100

    send private message

--- src/main.sh
+++ src/main.sh
@@ -2,7 +2,8 @@ source "$PR_SIZE_LABELER_HOME/src/rocket.sh"
 
 main(){
 
-   rocket::sendNotification $1 $2
+   rocket::sendGroupMessage $1 $2
+   rocket::sendPrivateMessage $3 $4 $5 $2
 
    exit $?
 }
--- src/rocket.sh
+++ src/rocket.sh
@@ -1,10 +1,28 @@
 #!/usr/bin/env bash
 
 ROCKET_CHAT_URL="https://chat.bulevip.com"
+URL_LOGIN="${ROCKET_CHAT_URL}/api/v1/login"
+SEND_MESSAGE_URL="${ROCKET_CHAT_URL}/api/v1/chat.postMessage"
 
-rocket::sendNotification(){
+rocket::sendGroupMessage(){
 	msg="$2"
   ROCKET_CHAT_HOOK="$1"
 	data="{\"text\":\"${msg}\",\"username\": \"Github Bot\"}"
 	curl -H "Content-type:application/json" "{$ROCKET_CHAT_URL}/hooks/{$ROCKET_CHAT_HOOK}" -d "$data"
+}
+
+
+rocket::sendPrivateMessage(){
+  msg="$4"
+  TO="@$3"
+
+  loginData="{\"user\":\"$1\",\"password\":\"$2\"}"
+  data="{\"channel\":\"${TO}\",\"text\":\"${msg}\",\"username\": \"Github Bot\",\"avatar\":\"https://i.pinimg.com/originals/bd/5c/46/bd5c46c4c1579c56c60920301ea3e642.png\"}"
+  login=$(curl -s -H "Content-type:application/json" "{$URL_LOGIN}" -d "$loginData")
+  userData=$(echo "$login" | jq '.data')
+  userId=$(echo "$userData" | jq -r '.userId')
+  token=$(echo "$userData" | jq -r '.authToken')
+
+  response=$(curl -s -H "Content-type:application/json" --header "X-User-Id: "$userId --header "X-Auth-Token: "$token "{$SEND_MESSAGE_URL}" -d "$data")
+  echo {$response}
 }
